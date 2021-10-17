e8b900ca75183fc86b33d65344be52ad097f8fbf
claudiabrito96 <claudia.brito@ufl.edu>
2021-10-17 19:19:04 -0400

    Finished LoginForm component and added it to the LogIn page

--- src/Pages/LogIn.js
+++ src/Pages/LogIn.js
@@ -1,34 +1,12 @@
 import React from 'react';
+import LoginForm from "../Components/LoginForm"
 
 class LogIn extends React.Component{
-    state={
-        email: ' ',
-        pwd: ' '
-    }
-
-    handleChange = (e) =>{
-        const {name,value} = e.target
-        this.setState({[name]:value})
-    }
-
-    handleSubmit = (e) =>{
-        //e.preventDefault()
-    }
-    render(){
-        return(
-            <div>
-                <form onSubmit = {this.handleSubmit}>
-                    <div>
-                    <input type='email' name='email' placeholder='email' required onChange={this.handleChange}/>
-                    </div>
-                    <div>
-                    <input type='password' name='pwd' placeholder='password' required oncChange={this.handleChange}/>
-                    </div>
-                    <button onSubmit={this.handleSubmit}>Log In</button>
-                </form> 
-            </div>
+    render (){
+        return (
+         <LoginForm/> 
         )
     }
 }
 
-export default LogIn;
+export default LogIn
