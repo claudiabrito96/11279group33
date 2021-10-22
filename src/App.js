6f06b3add62769faa9d2ccbf592d8f6de25a71ee
Julia Chancey <julia@Julias-MacBook-Air-2.local>
2021-10-22 14:28:17 -0400

    form for registering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
+import "semantic-ui-css/semantic.min.css";
 import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <BrowserRouter>
+    <Switch>
+        <Route exact path='/' component={Register} />
+      </Switch>
+    </BrowserRouter>
     </div>
+
   );
 }
 
