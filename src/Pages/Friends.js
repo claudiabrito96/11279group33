6b5f60107988649cb09f17fcde340b34a84b8aee
danielcasto <32224738+danielcasto@users.noreply.github.com>
2021-11-15 11:57:30 -0500

    Made a table on friends tab and created css file

--- src/Pages/Friends.js
+++ src/Pages/Friends.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import './Friends.css'
 
 
 function Friends() {
   return (
     <>
       <div>
-        Friends stuff
+        <table>
+            <tr>
+                <th>Friend</th>
+                <th>Energy Usage Today</th>
+            </tr>
+            <tr>
+                <td>Daniel Casto</td>
+                <td>50</td>
+            </tr>
+            <tr>
+                <td>Taise Miyazumi</td>
+                <td>51</td>
+            </tr>
+            <tr>
+                <td>Claudia Brito</td>
+                <td>55</td>
+            </tr>
+            <tr>
+                <td>Julia Chancey</td>
+                <td>49</td>
+            </tr>
+        </table>
       </div>
     </>
   );
